08beba3e629c7e9c374a3e2ff66c012418704cf4
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-11 23:46:44 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,8 @@
-#write out your code here
-
-def least_coins(cents)
-
-#Code your answer here!
+def least_coins 
+coins types= {:quarter=>0 :dime=>0 :nickel=>0 :penny=>0}
+elsif
+x=29
 
 end
+puts least_coins
+
